91745d6f31459d99aef4f107a28ac29bcf59f611
SteveReid <steve@appby.design>
2022-08-17 16:08:07 +0100

    Add remaining crud operations to apiSlice createApi endpoints

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -13,8 +13,34 @@ export const apiSlice = createApi({
   endpoints: builder => ({
     getTodos: builder.query<Todo, void>({
       query: () => '/todos'
+    }),
+    addTodo: builder.mutation<Todo, void>({
+      query: todo => ({
+        url: '/todos',
+        method: 'POST',
+        body: todo
+      })
+    }),
+    updateTodo: builder.mutation<Todo, Todo>({
+      query: todo => ({
+        url: `/todos/${todo.id}`,
+        method: 'PATCH',
+        body: todo
+      })
+    }),
+    deleteTodo: builder.mutation<void, Todo>({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: 'DELETE',
+        body: id
+      })
     })
   })
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation
+} = apiSlice;

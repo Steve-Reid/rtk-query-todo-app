4836c77ad5150f54a4f857616a408570a670d5b4
SteveReid <steve@appby.design>
2022-08-17 14:18:45 +0100

    Add api slice and provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { apiSlice } from '@features/api/apiSlice';
 import App from './App';
 import './index.css';
 
@@ -10,8 +10,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ApiProvider api={apiSlice}>
       <App />
-    </Provider>
+    </ApiProvider>
   </React.StrictMode>
 );
